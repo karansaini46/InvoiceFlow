81cf8fbfa6cc8c9f163f36b76a8cd7fa6bbfbafa
karansaini47 <karan0saini23@gmail.com>
2026-06-02 13:28:19 +0530

    Fix: redirect logged-in users from root to dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,7 +18,14 @@ import { LandingPage } from "@/pages/LandingPage";
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route
+        path="/"
+        element={
+          <PublicRoute>
+            <LandingPage />
+          </PublicRoute>
+        }
+      />
 
       <Route
         path="/login"
